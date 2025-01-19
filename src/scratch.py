1458d78b4829fa9186ca4a5426552b4a57987c48
Tanay Deshmukh <tanaydeshmukh96@gmail.com>
2025-01-19 18:07:02 +0100

    temp file deleted

--- src/scratch.py
+++
@@ -1,9 +0,0 @@
-from vector_store.embedder import SentenceTransformerEmbedder, HFAPIEmbedder
-
-embedder = HFAPIEmbedder(model_identifier="nvidia/NV-Embed-v2")
-
-embedding = embedder.encode(["This is a test sentence"])
-
-print(embedding.shape)
-
-pass
